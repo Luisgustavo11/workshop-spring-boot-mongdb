5e289305cff11f764c7283e94a0fceae25989ad4
Luisgustavo11 <luisgustavordp@gmail.com>
2022-01-31 22:51:26 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/luisgustavo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisgustavo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
