8dd031abdfd978617c2b14d553337b9fc6ba0995
Luisgustavo11 <luisgustavo@gmail.com>
2022-02-10 13:24:47 -0300

    Consulta simples com query methods

--- src/main/java/com/luisgustavo/workshopmongo/services/PostService.java
+++ src/main/java/com/luisgustavo/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.luisgustavo.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luisgustavo.workshopmongo.domain.Post;
-import com.luisgustavo.workshopmongo.domain.User;
 import com.luisgustavo.workshopmongo.repository.PostRepository;
 import com.luisgustavo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -19,5 +19,8 @@ public class PostService {
 	public Post findById(String id) {
 	    Optional<Post> user = repo.findById(id);
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
-	}	
+	}
+	public List<Post> findByTitle(String text) {
+	return repo.findByTitleContainingIgnoreCase(text);
+   }
 }
