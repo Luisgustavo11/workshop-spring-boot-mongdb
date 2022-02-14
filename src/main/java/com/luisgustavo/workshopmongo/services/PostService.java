866453aff9e5fa3f80514394c833b939f45e3275
Luisgustavo11 <luisgustavo@gmail.com>
2022-02-13 21:48:49 -0300

    Consulta simples com @Query

--- src/main/java/com/luisgustavo/workshopmongo/services/PostService.java
+++ src/main/java/com/luisgustavo/workshopmongo/services/PostService.java
@@ -21,6 +21,6 @@ public class PostService {
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
 	}
 	public List<Post> findByTitle(String text) {
-	return repo.findByTitleContainingIgnoreCase(text);
+	return repo.searchTitle(text);
    }
 }
